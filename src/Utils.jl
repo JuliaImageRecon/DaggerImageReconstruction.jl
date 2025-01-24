e92e4daf9e9bd8b4c480c36de443e6c99ffecc63
nHackel <nHackel@users.noreply.github.com>
2025-01-24 19:07:20 +0100

    Add loading of remote plan

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,1 +1,14 @@
 chunktype(::Dagger.Chunk{T}) where T = T
+
+export loadDaggerPlan
+"""
+    loadDaggerPlan(args...; worker)
+  
+Load a `RecoPlan` from on a TOML according to `loadPlan(args...)` on the specified worker. The resulting `RecoPlan` is embedded within a `DaggerReconstructionAlgorithm`.
+"""
+
+function loadDaggerPlan(args...; worker)
+  plan = Dagger.@mutable worker = worker load_plan(args...)
+  params = RecoPlan(DaggerReconstructionParameter; worker = worker, algo = plan)
+  return RecoPlan(DaggerReconstructionAlgorithm; parameter = params)
+end
