d71135afc83bee0dc501381a2f1efd749dc10c98
nHackel <nHackel@users.noreply.github.com>
2025-03-24 19:26:31 +0100

    LoadDaggerPlan loads into IOBuffer

--- src/Utils.jl
+++ src/Utils.jl
@@ -2,14 +2,20 @@ chunktype(::Dagger.Chunk{T}) where T = T
 
 export loadDaggerPlan
 """
-    loadDaggerPlan(args...; worker)
+    loadDaggerPlan(filename; worker)
   
-Load a local `RecoPlan` from on a TOML according to `loadPlan(args...)` and move it to the specified worker. The resulting `RecoPlan` is embedded within a `DaggerReconstructionAlgorithm`.
+Read a local `RecoPlan` from `args`  and interpret it on the the specified worker. The resulting `RecoPlan` is embedded within a `DaggerReconstructionAlgorithm`.
 """
 
-function loadDaggerPlan(args...; worker)
-  local_plan = loadPlan(args...)
-  plan = Dagger.@mutable worker = worker local_plan
+function loadDaggerPlan(filename, modules; worker)
+  buffer = IOBuffer()
+  open(filename) do file
+    for line in readlines(file; keep = true)
+      write(buffer, line)
+    end
+  end
+  seekstart(buffer)
+  plan = Dagger.@mutable worker = worker loadPlan(buffer, modules)
   params = RecoPlan(DaggerReconstructionParameter; worker = worker, algo = plan)
   return RecoPlan(DaggerReconstructionAlgorithm; parameter = params)
 end
